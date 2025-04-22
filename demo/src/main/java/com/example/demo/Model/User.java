dcdb6905a1557b996de0be1b212fa2763abc90fd
SivabalanMayilmurugan <heysivabalan@gmail.com>
2025-04-22 15:05:21 +0530

    Second commit

--- demo/src/main/java/com/example/demo/Model/User.java
+++ demo/src/main/java/com/example/demo/Model/User.java
@@ -1,17 +1,16 @@
 package com.example.demo.Model;
 
-import org.hibernate.annotations.GeneratedColumn;
-import org.hibernate.id.factory.internal.AutoGenerationTypeStrategy;
-
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
     
     @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private long id;
 
     @Column
